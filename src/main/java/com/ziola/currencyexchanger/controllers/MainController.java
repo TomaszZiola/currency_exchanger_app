65d8da533028d738433317f4cf6ebc499c3130fb
tomek <tomek.ziola@gmail.com>
2022-02-02 21:06:59 +0100

    some small adjustments

--- src/main/java/com/ziola/currencyexchanger/controllers/MainController.java
+++ src/main/java/com/ziola/currencyexchanger/controllers/MainController.java
@@ -4,20 +4,21 @@ import com.ziola.currencyexchanger.dto.CurrencyInputDto;
 import com.ziola.currencyexchanger.dto.CurrencyOutputDto;
 import com.ziola.currencyexchanger.errors.CurrencyInputEmptyException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
-    @GetMapping
-    public CurrencyOutputDto exchangeGivenCurrency(@RequestBody CurrencyInputDto currencyInputDto) throws CurrencyInputEmptyException {
+    @PostMapping
+    public CurrencyOutputDto exchangeGivenCurrency(@RequestBody CurrencyInputDto currencyInputDto) {
 
-        checkIfCorrect(currencyInputDto);
+        CurrencyInputDto currencyInputTemp = checkIfCorrect(currencyInputDto);
 
         CurrencyOutputDto result = new CurrencyOutputDto.Builder()
                 .currencyToExchange("0")
-                .currencyExchanged("0")
+                .currencyExchanged(currencyInputDto.getCurrencyExchanged())
                 .amountToExchange("0")
                 .amountExchanged("0")
                 .build();
@@ -25,15 +26,8 @@ public class MainController {
         return result;
     }
 
-    private void checkIfCorrect(CurrencyInputDto currencyInputDto) throws CurrencyInputEmptyException {
+    private CurrencyInputDto checkIfCorrect(CurrencyInputDto currencyInputDto) {
 
-        if(currencyInputDto.getCurrencyExchanged().length() == 0)
-            throw new CurrencyInputEmptyException(currencyInputDto.getCurrencyExchanged());
-        else if(currencyInputDto.getCurrencyToExchange().isEmpty())
-            throw new CurrencyInputEmptyException(currencyInputDto.getCurrencyToExchange());
-        else if (currencyInputDto.getAmountToExchange().isEmpty())
-            throw new CurrencyInputEmptyException(currencyInputDto.getAmountToExchange());
-        else if(currencyInputDto.getAmountExchanged() == null)
-            throw new CurrencyInputEmptyException(currencyInputDto.getAmountExchanged());
+        return currencyInputDto;
     }
 }
